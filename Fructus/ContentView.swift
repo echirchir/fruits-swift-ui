c5c66890671c111a7234712be1f286935b18302d
Elisha Chirchir <elisha.java@gmail.com>
2023-05-29 19:46:28 +0300

    Initial Commit

---
+++ Fructus/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Elisha Chirchir on 29/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Elisha Chirchir on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
